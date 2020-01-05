c08031e7dfbbd4465d5d93c6f02dca7261d2f1a6
Steviecoaster <stephen@chocolatey.io>
2020-01-04 21:45:56 -0500

    Additions to pages  need pushed to main dashboard script.

--- src/dashboard.ps1
+++ src/dashboard.ps1
@@ -2,6 +2,6 @@ $root = Split-Path $MyInvocation.MyCommand.Definition
 $pages = Join-Path $root "Pages"
 Get-ChildItem $PSScriptRoot\Pages\*.ps1 -Recurse | % { . $_.FullName }
 
-$dashboard = New-UDDashboard -Title "Valdinger Home Library" -Pages @($search,$AddBook,$SearchLoan,$NewLoan,$ReturnLoan)
+$dashboard = New-UDDashboard -Title "Valdinger Home Library" -Pages @($Homepage,$search,$AddBook,$AddBook2,$SearchLoan,$NewLoan,$ReturnLoan) 
 
 Start-UDDashboard -Dashboard $dashboard -Port 10001 -Wait
